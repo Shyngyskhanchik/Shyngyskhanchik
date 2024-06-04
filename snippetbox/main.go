b744cfeda8bafa2ac5b9e97432018787ca6b8a6f
Gaiisa <gaisazhenis0@gmail.com>
2024-06-04 09:20:48 +0500

    urok 3

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -7,7 +7,7 @@ import (
 
 // Обработчик главной страницы.
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Привет из Snippetbox"))
+	w.Write([]byte("Привет из Snippetbox my namwe is Shyngyskhan"))
 }
 
 // Обработчик для отображения содержимого заметки.
